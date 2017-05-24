597adc8e1287d9c9645e02c3c3e77bbf2035caae
Petr Jasicek <pjasicek@gmail.com>
2017-05-24 14:24:34 -0400

    Added linux config

--- Scripts/package_rpm.sh
+++ Scripts/package_rpm.sh
@@ -22,6 +22,7 @@ ensure_exists $BINARY_BUILD_DIR/ASSETS.ZIP
 ensure_exists $BINARY_BUILD_DIR/console02.tga
 ensure_exists $BINARY_BUILD_DIR/clacon.ttf
 ensure_exists $BINARY_BUILD_DIR/SAVES.XML
+ensure_exists $BINARY_BUILD_DIR/config_linux.xml
 
 mkdir -p $TARGET_BINARY_DIR
 mkdir -p $TARGET_ASSETS_DIR
@@ -32,5 +33,4 @@ cp $BINARY_BUILD_DIR/ASSETS.ZIP $TARGET_ASSETS_DIR
 cp $BINARY_BUILD_DIR/console02.tga $TARGET_ASSETS_DIR
 cp $BINARY_BUILD_DIR/clacon.ttf $TARGET_ASSETS_DIR
 cp $BINARY_BUILD_DIR/SAVES.XML $TARGET_ASSETS_DIR
-
-
+cp $BINARY_BUILD_DIR/config_linux.xml $TARGET_ASSETS_DIR
